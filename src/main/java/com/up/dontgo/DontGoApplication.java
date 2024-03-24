892c7c690ab00db56526e40dcd31b5604b1d6997
lmQAQml <lmQAQml@163.com>
2024-03-24 09:00:43 +0800

    初始化项目

---
+++ src/main/java/com/up/dontgo/DontGoApplication.java
@@ -0,0 +1,13 @@
+package com.up.dontgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DontGoApplication.class, args);
+    }
+
+}
